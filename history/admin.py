8f4e86f6ac1fa2665002620612c139159866bb55
Ahsan Shafiq <ahsanshafiq742@gmail.com>
2018-02-06 19:01:21 +0500

    zip file added

--- history/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from simple_history.admin import SimpleHistoryAdmin
-from .models import Publication, Article
-
-admin.site.register(Publication, SimpleHistoryAdmin)
-admin.site.register(Article, SimpleHistoryAdmin)
